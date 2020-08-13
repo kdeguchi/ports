4084aad4be0a77c13684ebb6717cd8239ebb22b8
kdeguchi <kdeguchi@sz.tokoha-u.ac.jp>
2020-08-13 09:24:46 +0900

    iconv patch

--- devel/glibmm/files/patch-glib_glibmm_ustring.cc
+++ devel/glibmm/files/patch-glib_glibmm_ustring.cc
@@ -1,15 +1,15 @@
---- glib/glibmm/ustring.cc.orig	2015-01-20 21:09:51.000000000 +0900
-+++ glib/glibmm/ustring.cc	2015-11-24 23:56:24.643136000 +0900
-@@ -1265,7 +1265,7 @@
+--- glib/glibmm/ustring.cc.orig	2017-08-30 14:13:30 UTC
++++ glib/glibmm/ustring.cc
+@@ -1364,7 +1364,7 @@ ustring::FormatStream::to_string() const
  #ifdef GLIBMM_HAVE_WIDE_STREAM
    const std::wstring str = stream_.str();
  
--# if defined(__STDC_ISO_10646__) && SIZEOF_WCHAR_T == 4
-+# if defined(__STDC_UTF_32__) && SIZEOF_WCHAR_T == 4
+-#if defined(__STDC_ISO_10646__) && SIZEOF_WCHAR_T == 4
++#if defined(__STDC_UTF_32__) && SIZEOF_WCHAR_T == 4
    // Avoid going through iconv if wchar_t always contains UCS-4.
    glong n_bytes = 0;
-   const ScopedPtr<char> buf (g_ucs4_to_utf8(reinterpret_cast<const gunichar*>(str.data()),
-@@ -1349,7 +1349,7 @@
+   const auto buf = make_unique_ptr_gfree(g_ucs4_to_utf8(
+@@ -1452,7 +1452,7 @@ operator>>(std::wistream& is, ustring& utf8_string)
    std::wstring wstr;
    is >> wstr;
  
@@ -17,13 +17,13 @@
 +#if defined(__STDC_UTF_32__) && SIZEOF_WCHAR_T == 4
    // Avoid going through iconv if wchar_t always contains UCS-4.
    glong n_bytes = 0;
-   const ScopedPtr<char> buf (g_ucs4_to_utf8(reinterpret_cast<const gunichar*>(wstr.data()),
-@@ -1380,7 +1380,7 @@
+   const auto buf = make_unique_ptr_gfree(g_ucs4_to_utf8(
+@@ -1483,7 +1483,7 @@ operator<<(std::wostream& os, const ustring& utf8_stri
  {
-   GError* error = 0;
+   GError* error = nullptr;
  
 -#if defined(__STDC_ISO_10646__) && SIZEOF_WCHAR_T == 4
 +#if defined(__STDC_UTF_32__) && SIZEOF_WCHAR_T == 4
    // Avoid going through iconv if wchar_t always contains UCS-4.
-   const ScopedPtr<gunichar> buf (g_utf8_to_ucs4(utf8_string.raw().data(),
-                                                 utf8_string.raw().size(), 0, 0, &error));
+   const auto buf = make_unique_ptr_gfree(
+     g_utf8_to_ucs4(utf8_string.raw().data(), utf8_string.raw().size(), nullptr, nullptr, &error));
