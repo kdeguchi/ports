df0a1eb903ffcf633502fdd25efe817279249aac
kdeguchi <kdeguchi@sz.tokoha-u.ac.jp>
2021-06-10 11:37:48 +0900

    Follow the ports tree.

--- sysutils/conky/files/patch-doc_docgen.sh
+++ sysutils/conky/files/patch-doc_docgen.sh
@@ -9,8 +9,5 @@
 +xsltproc LOCALBASE/share/xsl/docbook/manpages/docbook.xsl config_settings.xml > config_settings.html && \
 +xsltproc LOCALBASE/share/xsl/docbook/manpages/docbook.xsl lua.xml > lua.html && \
  db2x_xsltproc -s man docs.xml -o docs.mxml && \
--db2x_manxml docs.mxml && \
-+db2x_manxml docs.mxml
+ db2x_manxml docs.mxml && \
  { echo ".TH CONKY 1 \"August 2005\" \"conky compiled August 2005\" \"User Commands\""; sed 1d < conky.1; } > conky.2 && \
- mv conky.2 conky.1 && \
- man ./conky.1 | col -b > README && \
