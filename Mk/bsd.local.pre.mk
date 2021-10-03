3fe1ee996908e397b708d440f9705828abbdafa0
kdeguchi <kdeguchi@sz.tokoha-u.ac.jp>
2021-10-04 04:43:12 +0900

    Delete Mk/Uses/compiler.mk

--- Mk/bsd.local.pre.mk
+++ Mk/bsd.local.pre.mk
@@ -1,16 +1,5 @@
-.if ${.CURDIR:M*/graphics/gegl}
-USES:=	${USES:S@compiler:c++14-lang@compiler:openmp@}
-.endif
-
 .if ${.CURDIR:M*/graphics/ImageMagick6}
 USES:=	${USES:S@pkgconfig:both@pkgconfig@}
-.if ${PORT_OPTIONS:MOPENMP}
-USES:=	${USES:S@c++11-lang@openmp@}
-.endif
-.endif
-
-.if ${.CURDIR:M*/multimedia/libxine}
-USES:=	${USES:S@compiler @compiler:openmp @W}
 .endif
 
 .if ${.CURDIR:M*/devel/p5-ExtUtils-PkgConfig}
