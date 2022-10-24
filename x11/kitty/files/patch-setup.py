0633b86069b8717fa746210cd8ec519471fc7643
kdeguchi <kdeguchi@sz.tokoha-u.ac.jp>
2022-10-24 10:59:05 +0900

    support libutemper

--- x11/kitty/files/patch-setup.py
+++ x11/kitty/files/patch-setup.py
@@ -33,7 +33,7 @@
      if src == 'kitty/parser_dump.c':
          return 'kitty/parser.c', ['DUMP_COMMANDS']
      if src == 'kitty/data-types.c':
-+        env.ldflags.append('-lulog')
++        env.ldflags.append('-lutempter')
          return src, get_vcs_rev_defines(env, src)
      with suppress(KeyError):
          return src, env.library_paths[src]
