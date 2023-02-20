aebafdde962789bf71fff68a87adf5ce25e0dcfb
kdeguchi <kdeguchi@sz.tokoha-u.ac.jp>
2023-02-20 13:12:44 +0900

    update kitty-0.27.1

--- x11/kitty/files/patch-kitty_child-monitor.c
+++ x11/kitty/files/patch-kitty_child-monitor.c
@@ -1,4 +1,4 @@
---- kitty/child-monitor.c.orig	2022-11-07 10:06:42 UTC
+--- kitty/child-monitor.c.orig	2023-02-07 10:40:29 UTC
 +++ kitty/child-monitor.c
 @@ -22,6 +22,7 @@
  #include <sys/socket.h>
@@ -8,7 +8,7 @@
  extern PyTypeObject Screen_Type;
  
  #if defined(__APPLE__) || defined(__OpenBSD__)
-@@ -239,6 +240,7 @@ add_child(ChildMonitor *self, PyObject *args) {
+@@ -273,6 +274,7 @@ add_child(ChildMonitor *self, PyObject *args) {
          return NULL;
      }
  #undef A
@@ -16,7 +16,7 @@
      INCREF_CHILD(add_queue[add_queue_count]);
      add_queue_count++;
      children_mutex(unlock);
-@@ -1190,6 +1192,7 @@ hangup(pid_t pid) {
+@@ -1241,6 +1243,7 @@ hangup(pid_t pid) {
  
  static void
  cleanup_child(ssize_t i) {
--- x11/kitty/files/patch-setup.py
+++ x11/kitty/files/patch-setup.py
@@ -1,6 +1,6 @@
---- setup.py.orig	2022-11-20 22:56:03 UTC
+--- setup.py.orig	2023-02-20 03:17:28 UTC
 +++ setup.py
-@@ -130,27 +130,11 @@ def libcrypto_flags() -> Tuple[List[str], List[str]]:
+@@ -126,27 +126,11 @@ def libcrypto_flags() -> Tuple[List[str], List[str]]:
      # Apple use their special snowflake TLS libraries and additionally
      # have an ancient broken system OpenSSL, so we need to check for one
      # installed by all the various macOS package managers.
@@ -31,11 +31,11 @@
  def at_least_version(package: str, major: int, minor: int = 0) -> None:
      q = f'{major}.{minor}'
      if subprocess.run([PKGCONFIG, package, f'--atleast-version={q}']
-@@ -528,6 +512,7 @@ def get_source_specific_defines(env: Env, src: str) ->
+@@ -526,6 +510,7 @@ def get_source_specific_defines(env: Env, src: str) ->
      if src == 'kitty/parser_dump.c':
          return 'kitty/parser.c', ['DUMP_COMMANDS']
      if src == 'kitty/data-types.c':
 +        env.ldflags.append('-lutempter')
-         return src, get_vcs_rev_defines(env, src)
-     with suppress(KeyError):
+         return src, [f'KITTY_VCS_REV="{get_vcs_rev_define()}"', f'WRAPPED_KITTENS="{wrapped_kittens()}"']
+     try:
          return src, env.library_paths[src]
