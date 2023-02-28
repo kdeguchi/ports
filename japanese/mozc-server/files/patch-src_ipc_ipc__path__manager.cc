35f884e9f3a2160d562efe366a1573aaf27a8226
kdeguchi <kdeguchi@sz.tokoha-u.ac.jp>
2023-02-28 16:39:55 +0900

    update mozc

--- japanese/mozc-server/files/patch-src_ipc_ipc__path__manager.cc
+++ japanese/mozc-server/files/patch-src_ipc_ipc__path__manager.cc
@@ -1,15 +1,14 @@
 --- src/ipc/ipc_path_manager.cc.orig	2023-01-23 19:19:19 UTC
 +++ src/ipc/ipc_path_manager.cc
-@@ -298,10 +298,10 @@ bool IPCPathManager::GetPathName(std::string *ipc_name
+@@ -298,9 +298,11 @@ bool IPCPathManager::GetPathName(std::string *ipc_name
    *ipc_name = kIPCPrefix;
  #endif  // _WIN32
  
--#ifdef __linux__
-+#ifdef __linux__ && !defined(__FreeBSD__)
+ #ifdef __linux__
++#if !defined(__FreeBSD__)
    // On Linux, use abstract namespace which is independent of the file system.
    (*ipc_name)[0] = '\0';
--#endif  // __linux__
-+#endif
++#endif  // !__FreeBSD__
+ #endif  // __linux__
  
    ipc_name->append(ipc_path_info_->key());
-   ipc_name->append(".");
