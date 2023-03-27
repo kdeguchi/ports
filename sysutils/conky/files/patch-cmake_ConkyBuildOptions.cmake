00af93d82f7f7fc0052c70a32d068001b7e26b02
kdeguchi <kdeguchi@sz.tokoha-u.ac.jp>
2023-03-27 10:52:27 +0900

    Follow the ports tree.

--- sysutils/conky/files/patch-cmake_ConkyBuildOptions.cmake
+++ sysutils/conky/files/patch-cmake_ConkyBuildOptions.cmake
@@ -5,7 +5,7 @@
  
  # Some standard options
 -set(SYSTEM_CONFIG_FILE "/etc/conky/conky.conf"
-+set(SYSTEM_CONFIG_FILE "LOCALBASE/etc/conky/conky.conf"
++set(SYSTEM_CONFIG_FILE "%%LOCALBASE%%/etc/conky/conky.conf"
      CACHE STRING "Default system-wide Conky configuration file")
  # use FORCE below to make sure this changes when CMAKE_INSTALL_PREFIX is
  # modified
