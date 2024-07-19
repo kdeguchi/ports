f426d85aa793e83f136031640a14384722a67198
kdeguchi <kdeguchi@sz.tokoha-u.ac.jp>
2024-07-19 09:52:55 +0900

    Update latest version.

--- x11/lightdm/files/patch-src_x-server-local.c
+++ x11/lightdm/files/patch-src_x-server-local.c
@@ -1,6 +1,6 @@
---- src/x-server-local.c.orig	2018-08-30 00:35:31 UTC
+--- src/x-server-local.c.orig	2024-06-20 04:53:29 UTC
 +++ src/x-server-local.c
-@@ -530,17 +530,13 @@ x_server_local_start (DisplayServer *display_server)
+@@ -542,17 +542,13 @@ x_server_local_start (DisplayServer *display_server)
          }
      }
  
