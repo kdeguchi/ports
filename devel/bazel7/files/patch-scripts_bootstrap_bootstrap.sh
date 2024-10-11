6660d068af1ffba65777cf24155eb41f779be869
kdeguchi <kdeguchi@sz.tokoha-u.ac.jp>
2024-10-11 09:34:55 +0900

    Update latest version.

--- devel/bazel7/files/patch-scripts_bootstrap_bootstrap.sh
+++ devel/bazel7/files/patch-scripts_bootstrap_bootstrap.sh
@@ -1,11 +1,11 @@
---- scripts/bootstrap/bootstrap.sh.orig	2020-10-07 21:26:37 UTC
+--- scripts/bootstrap/bootstrap.sh.orig	2024-10-11 00:06:41 UTC
 +++ scripts/bootstrap/bootstrap.sh
-@@ -33,7 +33,7 @@ fi
- 
+@@ -35,7 +35,7 @@ _BAZEL_ARGS="--spawn_strategy=standalone \
+ #  mirrored. See https://github.com/bazelbuild/bazel/pull/19549 for more context.
  _BAZEL_ARGS="--spawn_strategy=standalone \
        --nojava_header_compilation \
 -      --strategy=Javac=worker --worker_quit_after_build --ignore_unsupported_sandboxing \
 +      --strategy=Javac=standalone --ignore_unsupported_sandboxing --curses=no \
        --compilation_mode=opt \
-       --distdir=derived/distdir \
-       --java_toolchain=//src/java_tools/buildjar:bootstrap_toolchain \
+       --repository_cache=derived/repository_cache \
+       --repo_env=BAZEL_HTTP_RULES_URLS_AS_DEFAULT_CANONICAL_ID=0 \
