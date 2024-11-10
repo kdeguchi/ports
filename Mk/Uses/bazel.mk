47fc2e22712abc9b48886520ffaf1ba79b54851e
kdeguchi <kdeguchi@sz.tokoha-u.ac.jp>
2024-11-10 11:46:59 +0900

    cleanup bazel.mk

--- Mk/Uses/bazel.mk
+++ Mk/Uses/bazel.mk
@@ -82,39 +82,59 @@ IGNORE?= USES=bazel has invalid version number: ${BAZEL_VERSION}
 
 BAZEL_PORT=	devel/bazel${BAZEL_VERSION:N6}
 
-.  if ! empty(bazel_ARGS:Mworkspace)
+.  if !empty(bazel_ARGS:Nbzlmod)
 BAZEL_MODE=	workspace
-.  elseif ! empty(bazel_ARGS:Mbzlmod)
-BAZEL_MODE=	blzmod
+.  elseif !empty(bazel_ARGS:Nworkspace)
+BAZEL_MODE=	bzlmod
 .  else
 BAZEL_MODE=	workspace
 .  endif
 
 .  if ! empty(bazel_ARGS:Mbuild)
-BUILD_DEPENDS+=	${BAZEL_PORT}
+BUILD_DEPENDS+=	${BAZEL_CMD}:${BAZEL_PORT}
 .  elseif ! empty(bazel_ARGS:Mrun)
-RUN_DEPENDS+=	${BAZEL_PORT}
+RUN_DEPENDS+=	${BAZEL_CMD}:${BAZEL_PORT}
 .  else
-BUILD_DEPENDS+=	${BAZEL_PORT}
+BUILD_DEPENDS+=	${BAZEL_CMD}:${BAZEL_PORT}
 .  endif
 
 BAZEL_CMD=	${LOCALBASE}/bin/bazel
-BAZEL_DISTDIR=	${DISTDIR}/bazel
+BAZEL_DISTDIR=	${DISTDIR}/${BAZEL_DIST_SUBDIR}
+BAZEL_DIST_SUBDIR=	bazel
 BAZEL_BCR_URL?=	https://github.com/bazelbuild/bazel-central-registry.git
 BAZEL_BCR_DIR=	bcr
 BAZEL_BCR_LOCAL=	${BAZEL_DISTDIR}/${BAZEL_BCR_DIR}
 BAZEL_OUTPUT_BASE=	${WRKDIR}/bazel_base
 BAZEL_OUTPUT_USER_ROOT=	${WRKDIR}/bazel_out
+BAZEL_WRKSRC?=	${WRKSRC}
 
 BAZEL_BOOT=	--output_base=${BAZEL_OUTPUT_BASE} \
-		--output_user_root=${BAZEL_OUTPUT_USER_ROOT}
+		--output_user_root=${BAZEL_OUTPUT_USER_ROOT} \
+		--server_javabase=${JAVA_HOME}
 
-BAZEL_OPTS=	--distdir=${BAZEL_DISTDIR}
+BAZEL_COMMON_OPTS=	--distdir=${BAZEL_DISTDIR}
 
-.  if ${BAZEL_VERSION} > 5 && ${BAZEL_MODE:Mbzlmod}
+BAZEL_JAVA=	--action_env=PATH=${JAVA_HOME}/bin:${PATH} \
+		--tool_java_language_version=${JAVA_VERSION}
 
-BAZEL_BOOT+=	--enable_bzlmod
-BAZEL_OPTS+=	--registry=${BAZEL_BCR_LOCAL}
+USE_JAVA=	yes
+JAVA_VENDOR=	openjdk
+.if ${BAZEL_VERSION} == 7
+JAVA_VERSION=	21
+.else
+JAVA_VERSION=	11
+.endif
+JAVA_BUILD=	yes
+
+.  if ${BAZEL_VERSION} > 5 && ${BAZEL_MODE} == bzlmod
+
+BAZEL_COMMON_OPTS+=	--registry=file://${BAZEL_BCR_LOCAL} \
+		--enable_bzlmod
+BAZEL_MOD_OPTS+=	--output text --charset ascii
+BAZEL_BUILD_OPTS+=	--verbose_failures --sandbox_debug
+
+BAZEL_BUILD=	${BAZEL_CMD} ${BAZEL_BOOT} build \
+		${BAZEL_COMMON_OPTS} ${BAZEL_BUILD_OPTS}
 
 FETCH_DEPENDS+=	git:devel/git
 
@@ -122,30 +142,32 @@ _USES_fetch+=	800:fetch-bcr
 
 fetch-bcr:
 .    if ! exists(${BAZEL_BCR_LOCAL})
+	${MKDIR} ${BAZEL_DISTDIR}
 	cd ${BAZEL_DISTDIR} && \
 		git clone ${BAZEL_BCR_URL} ${BAZEL_BCR_DIR}
 .    else
 	cd ${BAZEL_BCR_LOCAL} && git pull || exit 0
 .    endif
 
+.    if !target(do-build)
+do-build:
+	cd ${BAZEL_WRKSRC} && ${BAZEL_BUILD} ${BAZEL_TARGET}
+.    endif
+
 bazel-modules: configure
-	cd ${WRKSRC} && \
-		${BAZEL_CMD} ${BAZEL_BOOT} mod show_repo $$( cd ${BUILD_WRKSRC} && \
-			bazel ${BAZEL_BOOT} mod graph \
-				--output text --charset ascii \
-				${BAZEL_OPTS} | \
+	cd ${BAZEL_WRKSRC} && \
+		${BAZEL_CMD} ${BAZEL_BOOT} mod show_repo $$( cd ${BAZEL_WRKSRC} && \
+			${BAZEL_CMD} ${BAZEL_BOOT} mod graph \
+				${BAZEL_COMMON_OPTS} ${BAZEL_MOD_OPTS} | \
 				${SED} -E 's/[_]+//;s/\|//g;s/^ +//;s/ .*//;' | \
 				${GREP} '@[0-9]' | sort -u ) \
-			--output text --charset ascii \
-			${BAZEL_COMMON_OPTS} | \
+			${BAZEL_COMMON_OPTS} ${BAZEL_MOD_OPTS} | \
 		${AWK} -F / ' / urls = / { \
 			sub(/.* urls = \["/, ""); sub(/"\],/, ""); \
 			group=$$5; gsub(/-/, "_", group); \
 			printf( "MASTER_SITES+=\t%s/%s/%s/%s/%s/%s/%s/%s/:%s\n", $$1, $$2, $$3, $$4, $$5, $$6, $$7, $$8, group ); \
 			printf( "DISTFILES+=\t%s:%s\n", $$9, group ); \
 		} ' > ${MASTERDIR}/Makefile.MASTER_SITES
-
 .  endif
 
-
-.endif
+.endif # !defined(_INCLUDE_USES_BAZEL_MK)
