962451cef9e6929e5275dfeabaded47be4520277
kdeguchi <kdeguchi.stem@gmail.com>
2026-01-06 11:53:30 +0900

    https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=292202

--- ports-mgmt/sccache-overlay/files/Mk/bsd.overlay.mk
+++ ports-mgmt/sccache-overlay/files/Mk/bsd.overlay.mk
@@ -1,44 +1,44 @@
-.if !defined(_SCCACHE_OVERLAY_INCLUDED) && !defined(NO_SCCACHE)
+.if !defined(_SCCACHE_OVERLAY_INCLUDED) && !defined(NO_CCACHE)
 _SCCACHE_OVERLAY_INCLUDED=	yes
 
-SCCACHE_SIZE?=		16G
+SCCACHE_SERVER_UDS?=	/tmp/sccache-overlay.socket
+SCCACHE_CONF?=		${SCCACHE_DIR}/config
 SCCACHE_IDLE_TIMEOUT?=	0
-SCCACHE_UNIX_LISTEN?=	/tmp/sccache-overlay.socket
 
-_SCCACHE_LIBS=	${LOCALBASE}/share/sccache/overlay/lib
-.for odir in ${OVERLAYS}
-.  if exists(${odir}/bin/sccache)
-SCCACHE_BIN?=	${odir}/bin/sccache
-_SCCACHE_LIBS=	${odir}/lib
+.  if exists(/ccache/bin/sccache)
+SCCACHE_BIN?=	/ccache/bin/sccache
+.  else
+.    for overlay in ${OVERLAYS}
+.      if exists(${overlay}/bin/sccache)
+SCCACHE_BIN?=	${overlay}/bin/sccache
+.      break
+.      endif
+.    endfor
 .  endif
-.endfor
 SCCACHE_BIN?=	${LOCALBASE}/share/sccache/overlay/bin/sccache
-.if !exists(${SCCACHE_BIN})
+.  if !exists(${SCCACHE_BIN})
 IGNORE=		could not find sccache: ${SCCACHE_BIN}
-.endif
+.  endif
 
-.if defined(CCACHE_DIR)
+.  ifdef CCACHE_DIR
 SCCACHE_DIR?=	${CCACHE_DIR}/sccache
-.endif
-.if !defined(SCCACHE_DIR)
+.  endif
+.  ifndef SCCACHE_DIR
 IGNORE=		SCCACHE_DIR not set
-.endif
+.  endif
 
-_SCCACHE_ENV=	RUSTC_WRAPPER="${SCCACHE_BIN}" \
-		SCCACHE_CACHE_SIZE="${SCCACHE_SIZE}" \
-		SCCACHE_DIR="${SCCACHE_DIR}" \
+_SCCACHE_ENV=	SCCACHE_SERVER_UDS="${SCCACHE_SERVER_UDS}" \
+		SCCACHE_CONF="${SCCACHE_CONF}" \
 		SCCACHE_IDLE_TIMEOUT="${SCCACHE_IDLE_TIMEOUT}" \
-		SCCACHE_MAX_FRAME_LENGTH=104857600 \
-		SCCACHE_UNIX_LISTEN="${SCCACHE_UNIX_LISTEN}"
+		RUSTC_WRAPPER="${SCCACHE_BIN}"
 CONFIGURE_ENV+=	${_SCCACHE_ENV}
 MAKE_ENV+=	${_SCCACHE_ENV}
 
 _USES_configure+=	250:sccache-start
-_USES_stage+=		950:sccache-stats
+_USES_stage+=		996:sccache-stats
 
 sccache-start:
 	@${ECHO_MSG} "==> Starting sccache"
-	@${LN} -Fs ${_SCCACHE_LIBS} /tmp/sccache-overlay
 	@${SETENV} ${_SCCACHE_ENV} ${SCCACHE_BIN} --start-server || ${TRUE}
 	@${SETENV} ${_SCCACHE_ENV} ${SCCACHE_BIN} --show-stats
 
