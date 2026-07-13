a82a7895e9d1a2177b16c2a9de3619caa77a6571
kdeguchi <kdeguchi.stem@gmail.com>
2026-07-14 08:44:56 +0900

    Follow the ports tree.

--- x11/kitty/files/patch-setup.py
+++ x11/kitty/files/patch-setup.py
@@ -1,4 +1,4 @@
---- setup.py.orig	2025-07-16 03:26:48 UTC
+--- setup.py	2025-12-16 12:38:37 UTC
 +++ setup.py
 @@ -780,6 +780,7 @@ def get_source_specific_defines(env: Env, src: str) ->
      if src == 'kitty/data-types.c':
@@ -8,11 +8,12 @@
          return src, [], [f'KITTY_VCS_REV="{env.vcs_rev}"', f'WRAPPED_KITTENS="{wrapped_kittens()}"']
      if src.startswith('3rdparty/base64/'):
          return src, ['3rdparty/base64',], base64_defines(env.binary_arch.isa)
-@@ -1489,12 +1490,16 @@ def create_linux_bundle_gunk(ddir: str, args: Options)
+@@ -1489,12 +1490,17 @@ def create_linux_bundle_gunk(ddir: str, args: Options)
      in_src_launcher = base / (f'{libdir_name}/kitty/kitty/launcher/kitty')
      launcher = base / 'bin/kitty'
      skip_docs = False
 +    build_docs = os.getenv('BUILD_DOCS') == 'yes'
++    build_manpages = os.getenv('BUILD_MANPAGES') == 'yes'
      if not os.path.exists('docs/_build/html'):
          kitten_exe = os.path.join(os.path.dirname(str(launcher)), 'kitten')
          if os.path.exists(kitten_exe):
@@ -20,17 +21,20 @@
              make = 'gmake' if is_freebsd else 'make'
 -            run_tool([make, 'docs'])
 +            if build_docs:
-+                run_tool([make, 'docs'])
-+            else:
++                run_tool([make, 'html'])
++            if build_manpages:
 +                run_tool([make, 'man'])
          else:
              if args.skip_building_kitten:
                  skip_docs = True
-@@ -1505,7 +1510,8 @@ def create_linux_bundle_gunk(ddir: str, args: Options)
+@@ -1504,8 +1510,10 @@ def create_linux_bundle_gunk(ddir: str, args: Options)
+             else:
                  raise SystemExit(f'kitten binary not found at: {kitten_exe}')
      if not skip_docs:
-         copy_man_pages(ddir)
+-        copy_man_pages(ddir)
 -        copy_html_docs(ddir)
++        if build_manpages:
++            copy_man_pages(ddir)
 +        if build_docs:
 +            copy_html_docs(ddir)
      for (icdir, ext) in {'256x256': 'png', 'scalable': 'svg'}.items():
