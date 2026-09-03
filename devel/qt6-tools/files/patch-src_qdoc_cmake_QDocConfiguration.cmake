efb03ccfe9c514f7dd2141803443ca6182087ff6
kdeguchi <kdeguchi.stem@gmail.com>
2026-09-03 14:43:31 +0900

    Follow the ports tree.

--- devel/qt6-tools/files/patch-src_qdoc_cmake_QDocConfiguration.cmake
+++ devel/qt6-tools/files/patch-src_qdoc_cmake_QDocConfiguration.cmake
@@ -1,15 +1,16 @@
---- src/qdoc/cmake/QDocConfiguration.cmake.orig	2026-05-08 01:19:07 UTC
+Supply the version of LLVM we are intending to use via QDOC_USER_CLANG_VERSION,
+a custom CMake varible derived from LLVM_VERSION in the port Makefile instead of
+iterating through all potential candidates. This avoids mismatches during the
+configure stage when a user has a version of LLVM installed greater than what is
+set in DEFAULT_VERSIONS.
+
+PR: 287430
+
+--- src/qdoc/cmake/QDocConfiguration.cmake.orig	2025-09-25 00:29:00 UTC
 +++ src/qdoc/cmake/QDocConfiguration.cmake
-@@ -8,8 +8,11 @@ set(QDOC_SUPPORTED_CLANG_VERSIONS
- 
- # List of explicitly supported Clang versions for QDoc
- set(QDOC_SUPPORTED_CLANG_VERSIONS
--    "21.1" "20.1" "19.1" "18.1" "17.0.6"
-+    "22.1" "22.0" "21.1" "20.1" "19.1" "18.1" "17.0.6"
+@@ -11,3 +11,5 @@ set(QDOC_SUPPORTED_CLANG_VERSIONS
+     "21.1" "20.1" "19.1" "18.1" "17.0"
  )
-+
+ 
 +# Try user-selected version of Clang first
 +list(PREPEND QDOC_SUPPORTED_CLANG_VERSIONS "${QDOC_USER_CLANG_VERSION}")
- 
- # Check for QDoc coverage dependencies
- find_program(LCOV_EXECUTABLE lcov DOC "Path to lcov executable")
